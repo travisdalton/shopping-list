4e7fc741b11767e0349c928c8bc6468af4bf0d03
Travis Dalton <dalton.travis@gmail.com>
2019-12-19 20:05:33 -0500

    new code for behavior when buttons clicked

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-$('#shopping-list-entry').click(function(event) {
-    let grocery = $('#shopping-item').val();
+function changeList() {
+    console.log('change made');
+    listenHere();
+}  
+ 
+ 
+function listenHere() {
+    $('shopping-list-entry').submit(function() {
+        let grocery = $('shopping-item').val();
+        let large = '<li><span class="shopping-item">' + grocery + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
+        $('#shopping-list').append(large);  
+    });
 
-    let large = '<li><span class="shopping-item">' + grocery + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
+    $("shopping-item-toggle").click(function() {
+        $("span.shoppting-item").toggleClass("shopping-item__checked")
+        $("shopping-item").css("text-decoration", "line-through")
+    });
 
-    $('#shopping-list').append(large);    
-            
+    $("shopping-item-delete").click(function() {
+        $("li").remove()
+    });
+}
 
-});
+ $(changeList);   
+
+
+
+
+
 
 
 
